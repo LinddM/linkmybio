b7cc6e78f0c892a815c31f0949ae8b257bb9484a
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-10-05 20:15:51 -0500

    using bulma

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+import yaml
+
+app = Flask(__name__)
+
+data_yaml = open('links.yaml').read()
+data = yaml.load(data_yaml)
+
+
+@app.route('/')
+def main():
+    images = {
+        "prof_img" : url_for('static', filename = 'profile/prof_img.jpg'),
+        "fb_img" : url_for('static', filename = 'social/fb.png'),
+        "ig_img" : url_for('static', filename = 'social/ig.png'),
+        "li_img" : url_for('static', filename = 'social/li.png'),
+        "git_img" : url_for('static', filename = 'social/git.png')
+    }
+    #links = data['links']
+    return render_template('home.html', images = images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
