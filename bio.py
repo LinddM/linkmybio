a6cd8a46257b95dbba9368c1621b8023bc0dff18
Maite de la Roca <delaroca.estrada21@gmail.com>
2019-10-07 19:11:32 -0500

    Styling :)

--- bio.py
+++ bio.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, url_for
 import yaml
+import glob
 
 app = Flask(__name__)
 
 data_yaml = open('links.yaml').read()
 data = yaml.load(data_yaml)
 
+print(data['links'])
 @app.route('/')
 def main():
     images = {
+        "chameleon" : url_for('static', filename = 'profile/chameleon.png'),
         "prof_img" : url_for('static', filename = 'profile/prof_img.jpg'),
-        "fb_img" : url_for('static', filename = 'social/facebook.png'),
-        "ig_img" : url_for('static', filename = 'social/instagram.png'),
-        "li_img" : url_for('static', filename = 'social/linkedin.png'),
-        "git_img" : url_for('static', filename = 'social/github.png'),
-        "gmail_img" : url_for('static', filename = 'social/gmail.png')
+        "facebook" : url_for('static', filename = 'social/facebook.png'),
+        "instagram" : url_for('static', filename = 'social/instagram.png'),
+        "linkedin" : url_for('static', filename = 'social/linkedin.png'),
+        "github" : url_for('static', filename = 'social/github.png'),
+        "gmail" : url_for('static', filename = 'social/gmail.png')
     }
 
     links = data['links']
